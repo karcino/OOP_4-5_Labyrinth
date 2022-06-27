a843bd92a8db711783b6388623436cbcfac1db13
David <50714711+schnavy@users.noreply.github.com>
2022-06-27 14:22:05 +0200

    Labyrinth und Knoten Klassen gefüllt

--- src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Knoten.java
+++ src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Knoten.java
@@ -1,4 +1,19 @@
 package de.uni_leipzig.oop_prakt.ss2022.team1.A4_5;
 
 public class Knoten {
+    private int nummer;
+    private Knoten[] nachbarknoten;
+
+    public Knoten(int nummer, Knoten[] nachbarknoten) {
+        this.nummer = nummer;
+        this.nachbarknoten = nachbarknoten;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public Knoten[] getNachbarknoten() {
+        return nachbarknoten;
+    }
 }
--- src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Labyrinth.java
+++ src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Labyrinth.java
@@ -1,4 +1,18 @@
 package de.uni_leipzig.oop_prakt.ss2022.team1.A4_5;
 
 public class Labyrinth {
+    private Knoten[] knoten;
+    private int[][] kanten;
+
+    public Labyrinth(int[][] kanten) {
+        this.kanten = kanten;
+    }
+
+    public Knoten[] getKnoten() {
+        return knoten;
+    }
+
+    public int[][] getKanten() {
+        return kanten;
+    }
 }
