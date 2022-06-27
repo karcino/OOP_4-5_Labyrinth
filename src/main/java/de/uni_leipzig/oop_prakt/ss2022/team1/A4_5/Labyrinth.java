25a83193d977e594f7e7da214cc5b08a76fddd31
Paul F <p.fiedler@posteo.de>
2022-06-27 15:14:50 +0200

    WegeFinder outlines

--- src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Labyrinth.java
+++ src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Labyrinth.java
@@ -1,7 +1,9 @@
 package de.uni_leipzig.oop_prakt.ss2022.team1.A4_5;
 
+import
+
 public class Labyrinth {
-    public Labyrinth() {
+    public Labyrinth(int[] kanten) {
     }
 
 }
--- src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/WegeFinder.java
+++ src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/WegeFinder.java
@@ -1,4 +1,33 @@
 package de.uni_leipzig.oop_prakt.ss2022.team1.A4_5;
 
+import java.util.*;
 public class WegeFinder {
+
+    private Labyrinth labyrinth;
+    private int Startknoten; // wird vom Filehandler als int übergeben§
+    private int Zielknoten;
+    private List<List<Knoten>> wege;
+
+    public WegeFinder(int[] Kanten, int startknoten, int zielknoten) {
+        Startknoten = startknoten;
+        Zielknoten = zielknoten;
+        //TODO Labyrinth mit Kanten[] intitialisieren
+        Labyrinth labyrinth1 = new Labyrinth(Kanten);
+    }
+
+    public List<List<Knoten>> getWege() {
+        return wege;
+    }
+
+    private List<Knoten> findeWeg(Knoten startKnoten, List<Knoten>) {
+
+        //TODO Rekursion implementiern
+
+
+
+    }
+    public void findeAlleWege() {
+        while () {
+
+    }
 }
