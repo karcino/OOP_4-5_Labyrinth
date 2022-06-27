84483b8c253c45ba3c0d22d83870dd68ae47cfa5
Paul F <p.fiedler@posteo.de>
2022-06-27 16:44:25 +0200

    Labyrinth begonnen

--- src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Labyrinth.java
+++ src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Labyrinth.java
@@ -2,12 +2,27 @@ package de.uni_leipzig.oop_prakt.ss2022.team1.A4_5;
 
 import de.uni_leipzig.oop_prakt.ss2022.team1.io.FileHandler;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Labyrinth {
     private Knoten[] knoten;
     private int[][] kanten;
 
     public Labyrinth(int[][] kanten) {
         this.kanten = kanten;
+        // Finde alle einzelknoten ohne Duplikate
+        Set<Knoten> tempSet = new HashSet<Knoten>();
+        for (int[] kante: kanten) {
+            for(int knoten : kante){
+                tempSet.add(new Knoten(knoten));
+            }
+        }
+        this.knoten = new Knoten[tempSet.size()];
+        for (int i = 0; i < kanten.length; i++) {
+
+        }
+
 
     }
 
--- src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Main.java
+++ src/main/java/de/uni_leipzig/oop_prakt/ss2022/team1/A4_5/Main.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        int[][] kanten = FileHandler.readIntArrayFromFile("labyrinth-1.graph");
 
         int[][] arr = FileHandler.readIntArrayFromFile("labyrinth-1.graph");
+
+        WegeFinder wegeFinder = new WegeFinder(arr, 1, 10);
 
         for (int[] a : arr) {
             System.out.println(Arrays.toString(a));
